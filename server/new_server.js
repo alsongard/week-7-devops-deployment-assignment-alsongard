fb984448d97a4c05a2a989939d348e54f1ed1230
alsongard <alsongard@gmail.com>
2025-07-25 23:12:55 +0300

    backend hosting setup

--- server/new_server.js
+++ server/new_server.js
@@ -7,26 +7,11 @@ console.log(PORT)
 const user = process.env.MONGO_USR;
 const password = process.env.MONGO_PASSWD;
 
-
-
-async function ConnectDB(){
-    try
-    {
-        await mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.f2dd9sx.mongodb.net/BugDB?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.f2dd9sx.mongodb.net/BugDB?retryWrites=true&w=majority&appName=Cluster0`)
+.then(()=>{
+        app.listen(PORT, ()=>{console.log(`Listening on port ${PORT}: http://localhost:${PORT}`)})
         console.log("Connected to MongoDB");
-       
-        if (!process.env.VERCEL)
-        {
-            app.listen(portNumber, ()=>{
-                console.log(`Server is listening on port ${portNumber}`);
-            })
-        }
-    }
-    catch(err)
-    {
+    })
+    .catch(err=>{
         console.log(`Error: ${err}`);
-        process.exit(1);
-    }
-}
-
-ConnectDB();
+    })
