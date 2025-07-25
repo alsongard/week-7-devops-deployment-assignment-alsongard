65b6c718f3f929d60e163a7079b8e69add392e71
alsongard <alsongard@gmail.com>
2025-07-25 19:34:21 +0300

    update server for hosting

--- server/new_server.js
+++ server/new_server.js
@@ -1,9 +1,32 @@
 const mongoose = require("mongoose");
 const cors = require("cors");
 const coroptions = {
-    origin:"https://bugtrackerwebapp.vercel.app",
-    methods:["POST", "GET", "PUT", "DELETE"],
-    credentials:true
+    origin: function (origin, callback){
+        if (process.env.NODE_ENV !== "production") // checks the environment at which t's running
+        {
+            return callback(null, true)
+        }
+        if (!origin)
+        {
+            callback(null,true); // remember second argument: returns true(permit domain) or false(permit domain) 
+        }
+        const allowedDomains = [
+            "https://bugtrackerwebapp.vercel.app",
+            "http://localhost:5173"
+        ]
+        if (allowedDomains.indexOf(origin) !== 1)// not equal the indexOf() method returns -1 if no value ns found in the array
+        {
+            return callback(null, true); // firstArgument: if we are expecting an error set this value as shown in else statemetn
+            // second argument: boolean value which indeicates if the origin is allowed(true) : on not allowed(false)
+        }
+        else
+        {
+            callback(new Error(`Origin: ${origin} not allowed by cors`));
+        }
+    },
+    methods:["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+    credentials:true,
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
 const app = require("./server")
 
@@ -14,7 +37,12 @@ console.log(PORT)
 const user = process.env.MONGO_USR;
 const password = process.env.MONGO_PASSWD;
 
-mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.f2dd9sx.mongodb.net/BugDB?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.f2dd9sx.mongodb.net/BugDB?retryWrites=true&w=majority&appName=Cluster0`, 
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    }
+)
 .then(()=>{
         app.listen(PORT, ()=>{console.log(`Listening on port ${PORT}: http://localhost:${PORT}`)})
         console.log("Connected to MongoDB");
