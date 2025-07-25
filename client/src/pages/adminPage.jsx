9ebea6e5f37d1d029e2e26f0f891de150f6f4094
alsongard <alsongard@gmail.com>
2025-07-25 16:54:03 +0300

    update for hosting

--- client/src/pages/adminPage.jsx
+++ client/src/pages/adminPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState , useEffect} from 'react';
 import axios from 'axios';
-import { set } from 'mongoose';
 function AdminPage() {
     const email = localStorage.getItem("email");
     const role = localStorage.getItem("role");
