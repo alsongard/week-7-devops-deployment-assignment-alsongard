05a39621ad44f8447ed49d15ea823886db509cbb
alsongard <alsongard@gmail.com>
2025-07-25 16:36:15 +0300

    Provider, configureStore and Protected Routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,25 @@ import BugPage from "./pages/report_bug_page.jsx";
 import AdminPage from "./pages/adminPage.jsx";
 import LoginAdmin from "./pages/login_admin.jsx";
 import LandingPage from "./components/landingComponent.jsx";
+import { Provider } from "react-redux"; 
+import reducerer from "./store/reducer.jsx";
+import {configureStore} from "@reduxjs/toolkit"
+import requireAuth from "./requireAuth.jsx";
+import SetAuthHeader from "./utils/setAuthHeader.jsx";
+
+const store = configureStore({reducer:reducerer}) 
+
+const BugProtected = requireAuth(BugPage);
+const AdminProtected = requireAuth(AdminPage);
+
+const token = localStorage.getItem("token")
+SetAuthHeader(token);
+
+if (token)
+{
+    store.dispatch({type:"LOGIN"})
+}
+
 export default function App()
 {
     const[darkMode, setDarkMode] = useState("");
@@ -20,19 +39,21 @@ export default function App()
       console.log(`bg is : ${bg}`)
     return (
         <div className={`${bg} dark:bg-slate-900 bg-white min-h-screen`}>
-            <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<div><Header/><Outlet/></div>}>
-                    <Route index element={<LandingPage />} />
-                    <Route path="register" element={<LoginReg />} />
-                    <Route path="services" element={<h1 className="text-2xl text-center dark:text-white">Our Services</h1>} />
-                    <Route path="*" element={<h1 className="text-2xl text-center dark:text-white">404 Not Found</h1>} />
-                    <Route path="bug" element={<BugPage />}/>
-                    <Route path='admin_login' element={<LoginAdmin/>}/>
-                    <Route path="admin_page" element={<AdminPage/>}/>
-                </Route>
-            </Routes>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<div><Header/><Outlet/></div>}>
+                        <Route index element={<LandingPage />} />
+                        <Route path="register" element={<LoginReg />} />
+                        <Route path="services" element={<h1 className="text-2xl text-center dark:text-white">Our Services</h1>} />
+                        <Route path="*" element={<h1 className="text-2xl text-center dark:text-white">404 Not Found</h1>} />
+                        <Route path="bug" element={<BugProtected />}/>
+                        <Route path='admin_login' element={<LoginAdmin/>}/>
+                        <Route path="admin_page" element={<AdminProtected/>}/>
+                    </Route>
+                </Routes>
+                </BrowserRouter>
+            </Provider>
         </div>
     )
 }
